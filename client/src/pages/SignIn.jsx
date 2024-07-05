09401ddc06484602e13802ccadd4edd1dcb5d3d0
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-07-05 07:35:50 -0700

    typing error fixed

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -43,13 +43,13 @@ const SignIn = () => {
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input type='email' id='email' placeholder='Email' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
         <input type='password' id='password' placeholder='Password' className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading' : 'Sign Up'}</button>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading' : 'Sign In'}</button>
         <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
         <Link to='/sign-up'>
-            <span className='text-blue-500'>Sign In</span>
+            <span className='text-blue-500'>Sign Up</span>
         </Link>
       </div>
       <p className='text-red-700 mt-5' >{error ? error.message || 'Something went wrong!' : ''}</p>
